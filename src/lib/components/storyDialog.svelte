a635df19e2b83e4f91e27c2f568e3ba83b10f507
the-byte-bender <cccefg2@gmail.com>
2025-07-22 21:53:34 +0300

    Fix: Automatically Focus heading in story dialog

--- src/lib/components/storyDialog.svelte
+++ src/lib/components/storyDialog.svelte
@@ -4,21 +4,33 @@
 -->
 
 <script lang="ts">
+    import { tick } from "svelte";
     import Modal from "./modal.svelte";
     import type { Story } from "../game/types";
 
     export let open = false;
     export let story: Story | null = null;
 
+    let headingElement: HTMLHeadingElement;
+
     const handleClose = () => {
         open = false;
     };
+
+    $: if (open && headingElement) {
+        // Wait for the next DOM update cycle.
+        tick().then(() => {
+            headingElement.focus();
+        });
+    }
 </script>
 
 <Modal bind:visible={open}>
     {#if story}
         <div role="document">
-            <h5 class="mb-3">Extra Content</h5>
+            <h5 class="mb-3" bind:this={headingElement} tabindex="-1">
+                Extra Content
+            </h5>
 
             <div class="mb-4">
                 {#each story.lines as line}
